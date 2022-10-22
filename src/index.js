cd8f4ed58a0a60f797d3a7e54760b0d6c754367a
Miguel Valdez <mavd22mv@gmail.com>
2022-10-22 15:51:41 -0400

    Configuring Craco

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { message } from "@/constants";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>Hello world!</h1>
+    <h1>{message}</h1>
   </React.StrictMode>
 );
