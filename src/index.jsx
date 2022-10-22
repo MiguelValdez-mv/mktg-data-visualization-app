b5271c82d14f48dca5b6333eb94ba43add67bda8
Miguel Valdez <mavd22mv@gmail.com>
2022-10-22 16:18:28 -0400

    Configuring Eslint and Prettier

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { message } from "@/constants";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>{message}</h1>
+    <h1>Hello world!</h1>
   </React.StrictMode>
 );
