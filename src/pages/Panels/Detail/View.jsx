3f1b6b5180332f5a260e23c599a71457a3e00794
Miguel Valdez <mavd22mv@gmail.com>
2023-02-14 10:42:49 -0400

    WIP

--- src/pages/Panels/Detail/View.jsx
+++ src/pages/Panels/Detail/View.jsx
@@ -1,13 +1,92 @@
+import PropTypes from "prop-types";
+
+import { IconAdd } from "@/assets/svgs/IconAdd";
+import { IconMenuRight } from "@/assets/svgs/IconMenuRight";
+import { IconSettings } from "@/assets/svgs/IconSettings";
+import { Link } from "@/components/atoms/Link";
+import { Text } from "@/components/atoms/Text";
+import { Button } from "@/components/atoms/buttons/Button";
+import { IconButton } from "@/components/atoms/buttons/IconButton";
+import { Content } from "@/components/layout/Content";
 import { Header } from "@/components/layout/Header";
+import { Menu } from "@/components/layout/Menu";
+import { MenuOption } from "@/components/layout/Menu/MenuOption";
 import { Page } from "@/components/layout/Page";
+import { Row } from "@/components/layout/Row";
+import { Spacing } from "@/components/layout/Spacing";
 import { COPY } from "@/copy";
 
-function View() {
+function View({
+  panelSettingsPath,
+  isLargeScreen,
+  isLoading,
+  panelName,
+  currentUserIsAdmin,
+}) {
   return (
     <Page>
       <Header title={COPY["panels.detail.title"]} />
+      <Spacing bottom={8} />
+
+      <Content isLoading={isLoading}>
+        <Row className="justify-between items-center">
+          <Text subtitle bold>
+            {panelName}
+          </Text>
+
+          {isLargeScreen ? (
+            <Row>
+              <Link to={panelSettingsPath}>
+                <Button variant="outline-primary" startIcon={<IconSettings />}>
+                  {COPY["panels.detail.settings"]}
+                </Button>
+              </Link>
+
+              {currentUserIsAdmin && (
+                <>
+                  <Spacing left={2} />
+                  <Button startIcon={<IconAdd />}>
+                    {COPY["panels.detail.addWidget"]}
+                  </Button>
+                </>
+              )}
+            </Row>
+          ) : (
+            <Menu
+              trigger={
+                <IconButton primary>
+                  <IconMenuRight />
+                </IconButton>
+              }
+              position="bottom right"
+            >
+              {(close) => (
+                <>
+                  {currentUserIsAdmin && (
+                    <MenuOption startIcon={<IconAdd />} close={close}>
+                      {COPY["panels.detail.addWidget"]}
+                    </MenuOption>
+                  )}
+
+                  <MenuOption startIcon={<IconSettings />} close={close}>
+                    {COPY["panels.detail.settings"]}
+                  </MenuOption>
+                </>
+              )}
+            </Menu>
+          )}
+        </Row>
+      </Content>
     </Page>
   );
 }
+
+View.propTypes = {
+  panelSettingsPath: PropTypes.string.isRequired,
+  isLargeScreen: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  panelName: PropTypes.string,
+  currentUserIsAdmin: PropTypes.bool.isRequired,
+};
 
 export default View;
--- src/pages/Panels/Detail/useActions.js
+++ src/pages/Panels/Detail/useActions.js
@@ -1,3 +1,27 @@
-const useActions = () => {};
+import { useParams } from "react-router-dom";
+
+import { useAuth } from "@/hooks/auth/useAuth";
+import { useGetPanelById } from "@/hooks/panels/useGetPanelById";
+import { useDimensions } from "@/hooks/useDimensions";
+import { isAdminUser } from "@/utils/checkUserRole";
+
+const useActions = () => {
+  const { panelId } = useParams();
+  const { user } = useAuth();
+  const { isLargeScreen } = useDimensions();
+
+  const queryToGetPanelDetail = useGetPanelById({ id: panelId });
+  const { data: { name = "" } = {} } = queryToGetPanelDetail;
+
+  const currentUserIsAdmin = isAdminUser(user);
+
+  return {
+    panelSettingsPath: `/panels/${panelId}/settings`,
+    isLargeScreen,
+    isLoading: queryToGetPanelDetail.isLoading,
+    panelName: name,
+    currentUserIsAdmin,
+  };
+};
 
 export default useActions;
